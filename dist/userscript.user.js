e324ee025361cd31e322722af2448e0cf6117d3f
subframe7536 <1667077010@qq.com>
2023-09-05 17:20:19 +0800

    chore: release v1.0.15

--- dist/userscript.user.js
+++ dist/userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         全局滚动条美化 & 字体修改
 // @namespace    http://tampermonkey.net/
-// @version      1.0.14
+// @version      1.0.15
 // @author       subframe7536
 // @description  全局字体美化，滚动条美化，支持自定义字体、自定义规则
 // @license      MIT
@@ -265,6 +265,7 @@
     );
   }
   function addSansFontDefault() {
+    addCSS("body", `font-family: ${BASE_CONFIG.MONO}`);
     addCSS(
       `body :not(${sansExcludeSelector.join(",")})`,
       [
@@ -432,6 +433,7 @@
       });
       return;
     }
+    addSansFontDefault();
     addCodeFont(...monospaceSelectors);
     addRootCSS("--d-border-radius", "0.25rem");
     addRootCSS("--font-mono", "monospace");
@@ -458,8 +460,6 @@
   init();
   window.onload = () => {
     setTimeout(() => {
-      addSansFontDefault();
-      loadStyles();
       if (!document.querySelector(`.${moduleName}`)) {
         logger.warn("未找到 userscript-mono 标签，重新加载");
         init();
