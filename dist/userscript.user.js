d9bb9b7f6d50a65a8617cd2143327420e51a0ee2
subframe7536 <1667077010@qq.com>
2023-09-15 22:17:56 +0800

    chore: release v1.0.19

--- dist/userscript.user.js
+++ dist/userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         全局滚动条美化 & 字体修改
 // @namespace    http://tampermonkey.net/
-// @version      1.0.18
+// @version      1.0.19
 // @author       subframe7536
 // @description  全局字体美化，滚动条美化，支持自定义字体、自定义规则
 // @license      MIT
