83f6fa0f41b362b31501860a07cc42088bc1cf18
subframe7536 <1667077010@qq.com>
2023-10-21 14:13:12 +0800

    feat: more lang in twitter

--- src/sites/twitter.ts
+++ src/sites/twitter.ts
@@ -3,5 +3,5 @@ import { BASE_CONFIG } from '../_head'
 import type { Site } from '.'
 
 export default [['twitter.com', 'x.com'], () => {
-  addCSS('div[lang=ja]', `font-family:${BASE_CONFIG.SANS}!important;`)
+  addCSS('div:is([lang=ja],[lang=en],[lang=ko])', `font-family:${BASE_CONFIG.SANS}!important;`)
 }] satisfies Site
