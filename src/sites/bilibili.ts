fd739d87f3424359d3218eb2d45c33563ed79717
subframe7536 <1667077010@qq.com>
2023-11-29 22:31:37 +0800

    feat: improve rules for bilibili

--- src/sites/bilibili.ts
+++ src/sites/bilibili.ts
@@ -5,5 +5,6 @@ export default [['www.bilibili.com', 't.bilibili.com', 'space.bilibili.com'], ()
   addSansFont(
     '.bili-comment.browser-pc *',
     '.video-page-card-small .card-box .info .title',
+    '.h .h-sign',
   )
 }] satisfies Site
