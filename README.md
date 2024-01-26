3255cca5ae78e8606d6acec166a9fa82397982ea
subframe7536 <1667077010@qq.com>
2024-01-26 20:37:10 +0800

    feat: improve rules of gitee

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 ## 全局滚动条美化 & 字体修改
 
-- 强制使用指定的 monospace 和 sans-serif 字体（可添加自定义）
+- 强制使用指定的 `monospace` 和 `sans-serif` 字体（可添加自定义）
   - Chrome/Edge: 设置 - 外观 - 自定义字体
 - 美化滚动条和字体显示
+
+## 提供自定义字体选项的原因
+
+在 `<html lang="ja">` 的环境中，浏览器可能会错误的显示 `sans-serif` 和 `monospace` 字体，可以通过 指定字体名称 解决
 
 ## 自定义配置
 
--- src/sites/gitee.ts
+++ src/sites/gitee.ts
@@ -5,4 +5,5 @@ export default ['gitee.com', () => {
   addCodeFont('.commit-id', 'textarea')
   addSansFont('button', '.ui:not(.iconfont)')
   addCSS('#git-header-nav #navbar-search-form', 'border-radius:4px')
+  addCSS('.markdown-body .markdown-code-block-copy-btn', 'font-family:iconfont!important')
 }] satisfies Site
