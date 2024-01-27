6aec5069def9b667a47ddcf5ca4ef64e832a7b80
subframe7536 <1667077010@qq.com>
2024-01-27 11:30:24 +0800

    chore: release v1.1.9

--- dist/userscript.user.js
+++ dist/userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         全局滚动条美化 & 字体修改
 // @namespace    http://tampermonkey.net/
-// @version      1.1.8
+// @version      1.1.9
 // @author       subframe7536
 // @description  全局字体美化，滚动条美化，支持自定义字体、自定义规则
 // @license      MIT
@@ -225,7 +225,7 @@
   function getSettingsVariable(key) {
     switch (key) {
       case "MONO":
-        return `var(--${monoVariableName},sans)`;
+        return `var(--${monoVariableName},monospace)`;
       case "MONO_SETTING":
         return `var(--${monoFeatureVariableName},"calt")`;
       case "SANS":
@@ -594,7 +594,7 @@ Monospace 字体特性: ${getMonoFeature()}
         logger.warn("未找到 CSS 变量属性，重新加载");
         __fontVariable();
       }
-    }, 100);
+    }, 500);
   };
 
 })();
