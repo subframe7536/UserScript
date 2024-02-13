d89603e7b4819e72290ccf73cb7e06c808c21a38
subframe7536 <1667077010@qq.com>
2024-02-13 23:32:18 +0800

    chore: release v1.1.10

--- dist/userscript.user.js
+++ dist/userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         全局滚动条美化 & 字体修改
 // @namespace    http://tampermonkey.net/
-// @version      1.1.9
+// @version      1.1.10
 // @author       subframe7536
 // @description  全局字体美化，滚动条美化，支持自定义字体、自定义规则
 // @license      MIT
@@ -338,6 +338,9 @@ Monospace 字体特性: ${getMonoFeature()}
   function addRootCSS(property, value) {
     styleArray.push(`:root{${property}:${value}}`);
   }
+  function addBodyVariable(property, value) {
+    styleArray.push(`body{--${property}:${value}}`);
+  }
   function addCSS(selectors, styles) {
     selectors = Array.isArray(selectors) ? selectors : [selectors];
     styles = Array.isArray(styles) ? styles : [styles];
@@ -345,9 +348,9 @@ Monospace 字体特性: ${getMonoFeature()}
   }
   let codeFontSelectors = [];
   function __fontVariable() {
-    setCssVariable(monoVariableName, `${getMono()},${getSans()}`);
-    setCssVariable(monoFeatureVariableName, getMonoFeature());
-    setCssVariable(sansVariableName, getSans());
+    addBodyVariable(monoVariableName, `${getMono()},${getSans()}`);
+    addBodyVariable(monoFeatureVariableName, getMonoFeature());
+    addBodyVariable(sansVariableName, getSans());
   }
   function __codeFont() {
     addCSS(
@@ -558,6 +561,7 @@ Monospace 字体特性: ${getMonoFeature()}
     }
     __sansFont();
     __codeFont();
+    __fontVariable();
     const monospaceVariableValue = "var(--script-mono)";
     addRootCSS("--font-mono", monospaceVariableValue);
     addRootCSS("--font-monospace", monospaceVariableValue);
@@ -572,7 +576,6 @@ Monospace 字体特性: ${getMonoFeature()}
     loadStyles(base);
   }
   init();
-  __fontVariable();
   loadSettingMenus();
   _GM_registerMenuCommand(`${getDebug() ? "关闭" : "开启"} Debug 模式并刷新页面`, () => {
     toggleDebug();
@@ -589,10 +592,6 @@ Monospace 字体特性: ${getMonoFeature()}
       if (!document.querySelector(`.${moduleName}`)) {
         logger.warn("未找到 userscript-mono 标签，重新加载");
         init();
-      }
-      if (!document.body.style.getPropertyValue(`--${monoVariableName}`)) {
-        logger.warn("未找到 CSS 变量属性，重新加载");
-        __fontVariable();
       }
     }, 500);
   };
