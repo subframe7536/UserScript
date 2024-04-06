a0afaa8ec7a98ccd524ca764bfb300847f391185
subframe7536 <1667077010@qq.com>
2024-04-06 20:51:45 +0800

    fix: fix github code block cursor

--- src/sites/github.ts
+++ src/sites/github.ts
@@ -3,7 +3,11 @@ import { getSettingsVariable } from '../settings'
 import type { Site } from '.'
 
 export default ['github.com', () => {
+  // manully setup font
   addCSS('#read-only-cursor-text-area', `font-family:${getSettingsVariable('MONO')}!important`)
+  addCSS('.code-navigation-cursor', 'display:none')
+  addCSS('#read-only-cursor-text-area', 'caret-color:var(--fgColor-default, var(--color-fg-default));')
+
   addRootCSS('--fontStack-monospace', getSettingsVariable('MONO'))
   addRootCSS('--fontStack-sansSerif', getSettingsVariable('SANS'))
   addRootCSS('--fontStack-system', getSettingsVariable('SANS'))
