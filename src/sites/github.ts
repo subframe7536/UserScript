c3ad127bde78c0a9e2cddd1da5a7f509b953d7fd
subframe7536 <1667077010@qq.com>
2024-04-06 20:47:15 +0800

    fix: fix github code block

--- src/sites/github.ts
+++ src/sites/github.ts
@@ -1,8 +1,9 @@
-import { addRootCSS } from '../utils'
+import { addCSS, addRootCSS } from '../utils'
 import { getSettingsVariable } from '../settings'
 import type { Site } from '.'
 
 export default ['github.com', () => {
+  addCSS('#read-only-cursor-text-area', `font-family:${getSettingsVariable('MONO')}!important`)
   addRootCSS('--fontStack-monospace', getSettingsVariable('MONO'))
   addRootCSS('--fontStack-sansSerif', getSettingsVariable('SANS'))
   addRootCSS('--fontStack-system', getSettingsVariable('SANS'))
