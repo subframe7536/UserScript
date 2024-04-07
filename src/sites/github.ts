fd050c6117c3340f4e62e51cb322ed70fde9a394
subframe7536 <1667077010@qq.com>
2024-04-07 20:15:49 +0800

    fix: fix github repo page

--- src/sites/github.ts
+++ src/sites/github.ts
@@ -5,6 +5,8 @@ import type { Site } from '.'
 export default ['github.com', () => {
   // manully setup font
   addCSS('#read-only-cursor-text-area', `font-family:${getSettingsVariable('MONO')}!important`)
+  addCSS('.markdown-body', `font-family:${getSettingsVariable('SANS')}!important`)
+  addCSS('body', `font-family:${getSettingsVariable('SANS')}!important`)
   addCSS('.code-navigation-cursor', 'display:none')
   addCSS('#read-only-cursor-text-area', 'caret-color:var(--fgColor-default, var(--color-fg-default));')
 
