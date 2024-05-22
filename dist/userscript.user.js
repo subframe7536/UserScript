9c435f1a2d70f76f9c418ed275e59c2a7537df02
subframe7536 <1667077010@qq.com>
2024-05-22 14:30:41 +0800

    chore: release v1.1.27

--- dist/userscript.user.js
+++ dist/userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         全局滚动条美化 & 字体修改
 // @namespace    http://tampermonkey.net/
-// @version      1.1.26
+// @version      1.1.27
 // @author       subframe7536
 // @description  全局字体美化，滚动条美化，支持自定义字体、自定义规则
 // @license      MIT
@@ -62,7 +62,8 @@
     [".font-mono", "[font-mono]", ".text-mono", "[text-mono]"].map((s) => `${s} *`),
     "#formattedJson *",
     ":is(.katex, .katex *)",
-    ":is(mjx-container, .MathJax) *"
+    ":is(mjx-container, .MathJax) *",
+    ".video-js *"
   ];
   const monospaceSelectors = [
     monacoCharWidthCheckElement,
