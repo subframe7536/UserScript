c2ec9f70b491564826c0b2190668aabab5998ed2
subframe7536 <1667077010@qq.com>
2025-02-18 14:44:33 +0800

    feat: mdn monospace

--- src/sites/mdn.ts
+++ src/sites/mdn.ts
@@ -3,5 +3,8 @@ import { getSettingsVariable } from '../settings'
 import { addCSS } from '../utils'
 
 export default ['developer.mozilla.org', () => {
-  addCSS(':root', `--font-body:${getSettingsVariable('SANS')}!important;`)
+  addCSS(':root', [
+    `--font-body:${getSettingsVariable('SANS')}!important;`,
+    `--font-code:${getSettingsVariable('MONO')}!important;`,
+  ])
 }] satisfies Site
