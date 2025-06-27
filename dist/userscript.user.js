70e25ff916d3d324afd4ab3f70aa5d6907e52f35
subframe7536 <1667077010@qq.com>
2025-06-27 14:36:13 +0800

    chore: release v1.2.15

--- dist/userscript.user.js
+++ dist/userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         全局滚动条美化 & 字体修改
 // @namespace    http://tampermonkey.net/
-// @version      1.2.14
+// @version      1.2.15
 // @author       subframe7536
 // @description  全局字体美化，滚动条美化，支持自定义字体、自定义规则
 // @license      MIT
@@ -596,7 +596,8 @@ Monospace 字体特性: ${getMonoFeature()}
       "textarea",
       "[id^=find-in-file-item]",
       ".react-code-size-details-in-header *",
-      ".blob-code-inner *"
+      ".blob-code-inner *",
+      ".commit-ref *"
     ], codeStyles);
     addCSS([
       ".markdown-body",
