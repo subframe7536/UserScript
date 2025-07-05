870a0874d72b4c1bab3a55679b9ff5b4e8df0083
subframe7536 <1667077010@qq.com>
2025-07-05 09:18:34 +0800

    chore: release v1.2.25

--- dist/userscript.user.js
+++ dist/userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         全局滚动条美化 & 字体修改
 // @namespace    http://tampermonkey.net/
-// @version      1.2.24
+// @version      1.2.25
 // @author       subframe7536
 // @description  全局字体美化，滚动条美化，支持自定义字体、自定义规则
 // @license      MIT
@@ -606,7 +606,9 @@ Monospace 字体特性: ${getMonoFeature()}
       "[class*=BranchName]",
       "samp",
       "kbd",
-      "tt"
+      "tt",
+      "#gist-share-url-sized-down",
+      ".file-info *"
     ], codeStyles);
     addCSS([
       ".markdown-body",
