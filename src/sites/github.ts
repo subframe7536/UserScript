91839d0c337ba2b97a68910765a4e2472f39f161
subframe7536 <1667077010@qq.com>
2025-07-05 09:14:57 +0800

    fix: github

--- src/sites/github.ts
+++ src/sites/github.ts
@@ -18,6 +18,7 @@ export default [current => current.endsWith('github.com'), () => {
     '.text-mono',
     '.text-mono *',
     'textarea',
+    'table.highlight *',
     '[id^=find-in-file-item]',
     '.react-code-size-details-in-header *',
     '.blob-code-inner *',
