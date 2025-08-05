6f1ba4f44f73bdd1b2a7927e807f3d346e5c1c54
subframe7536 <1667077010@qq.com>
2025-08-05 19:02:18 +0800

    chore: release v1.2.30

--- dist/userscript.user.js
+++ dist/userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         全局滚动条美化 & 字体修改
 // @namespace    http://tampermonkey.net/
-// @version      1.2.29
+// @version      1.2.30
 // @author       subframe7536
 // @description  全局字体美化，滚动条美化，支持自定义字体、自定义规则
 // @license      MIT
@@ -761,7 +761,7 @@ Monospace 字体特性: ${getMonoFeature()}
   window.onload = () => {
     setTimeout(() => {
       const list = document.documentElement.classList;
-      if (list.contains("theme-dark") || list.contains("dark")) {
+      if (isDark && (list.contains("theme-dark") || list.contains("dark"))) {
         addRootCSS("color-scheme", "dark");
       }
       loadStyles();
