9df654babe1939e72e6b41a16715fa5c4bd013ce
subframe7536 <1667077010@qq.com>
2026-05-27 16:21:11 +0800

    chore: release v1.2.39

--- dist/userscript.user.js
+++ dist/userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         全局滚动条美化 & 字体修改
 // @namespace    http://tampermonkey.net/
-// @version      1.2.38
+// @version      1.2.39
 // @author       subframe7536
 // @description  全局字体美化，滚动条美化，支持自定义字体、自定义规则
 // @license      MIT
@@ -535,6 +535,7 @@ Monospace 字体特性: ${getMonoFeature()}
   }];
   const __vite_glob_0_18 = ["sourcegraph.com", () => {
     addCodeFont(".FileDiffHunks-module__body *");
+    addCodeFont(".FileDiffHunks.hunkline *");
   }];
   const __vite_glob_0_19 = ["stackoverflow.com", () => {
     addCSS("body", ["--ff-sans:", "--ff-mono:monospace,"].map((s) => `${s}${getSans()}!important`));
